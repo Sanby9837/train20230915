3649a7ae038bb5e003441bb72c5eb8d57b78bee1
Sanby9837 <Sanbylai@gmail.com>
2023-09-17 18:14:07 +0800

    20230917 first

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,8 +2,5 @@ public class test {
     public static void main (String[] args){
         System.out.println("kkkkkkkk");
 
-        var k = 1;
-        var kk = 2;
-        var kkk = k + kk;
     }
 }
